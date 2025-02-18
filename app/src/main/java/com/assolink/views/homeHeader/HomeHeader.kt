2fef4b53aa70c94ef871ab4b5fd1697fb4c94c2b
Roger <remirjunior1999@gmail.com>
2025-02-18 20:53:18 +0100

    Ajout des cards des associations

---
+++ app/src/main/java/com/assolink/views/homeHeader/HomeHeader.kt
@@ -0,0 +1,7 @@
+package com.assolink.views.homeHeader
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class HomeHeader @JvmOverloads constructor(context: Context, attrs: AttributeSet?) : ConstraintLayout(context, attrs)
